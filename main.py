8c1cff280b33401409448b75acea3c83edc60bdb
unknown <baidehibasnet@gmail.com>
2024-11-04 17:36:40 -1200

    week 2- for loop, range

---
+++ main.py
@@ -0,0 +1,13 @@
+import math_utils
+import mathamatics.trigonometry
+from mathamatics import trigonometry #main method
+import mathamatics
+from mathamatics.trigonometry import sine
+
+sum = math_utils.add(1,2)
+diff = math_utils.subtract(1,2)
+print(sum, diff)
+pi=3.14
+sin_45 = mathamatics.trigonometry.sine(pi/4) #function
+print (sin_45)
+
---
+++ math_utils.py
@@ -0,0 +1,7 @@
+# math_utild.py
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
